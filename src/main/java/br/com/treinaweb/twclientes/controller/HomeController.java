4d126445313610ecb09f186fea3e60d80f8c7dc6
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-08-30 19:57:48 -0300

    8 - Um pouco mais sobre a camada de Controller

--- src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -1,15 +1,40 @@
 package br.com.treinaweb.twclientes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
+    }
+
+    @GetMapping("/mensagem")
+    public ModelAndView mensagem(ModelMap modelMap) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("mensagem");
+        modelAndView.addObject("mensagem", "Mensagem vinda do Servidor!");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/saudacao")
+    public ModelAndView saudacao(
+        @RequestParam(required = false, defaultValue ="TreinaWeb") String nome ) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("saudacao");
+        modelAndView.addObject("nome", nome);
+        return modelAndView;
     }
 }
