b63e6e448620026cfe2d1fda8ef36cda882110df
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-09-03 22:54:21 -0300

    12 - Excluindo Clientes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-<h3>Detalhes:</h3>
+<h3>Adaptações:</h3>
 1 - Ide: IntelliJ<br>
 2 - spring.datasource.url= jdbc:mysql://localhost:3306/twclientes?useTimezone=true&serverTimezone=UTC&createDatabaseIfNotExist=true<br>
 Utilizei a propriedade acima para criar o banco de dados caso não exista e também já defini o Timezone.<br>
@@ -7,5 +7,10 @@ Utilizei a propriedade acima para criar o banco de dados caso não exista e tamb
 4 - Implementei o Lombok para facilitar os gettters, setters, constructors e etc.<br>
 5 - Utilizei a anotação @RestController no lugar da anotação @Controller.<br>
 6 - Separei as responsabilidades do serviço utilizando o pattern "service". Sendo assim, implementei a camada service e a anotação @Service nas classes responsáveis pela lógica do serviço.<br>
+7 - Criei 2 diretórios: response.dto e request.form<br>
+8 - Criei a classe ClienteDto para controlar o que será exibido sobre o cliente<br>
+9 - Formatei a data de nascimento utilizando o ${#temporals.format(cliente.dataNascimento, 'dd/MM/yyyy')}
+10 - Para implementar a funcionalidade de detalhar, utilizei o método findById() no lugar do getOne() <br>
+
 
 
--- src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
@@ -3,7 +3,9 @@ package br.com.treinaweb.twclientes.controller;
 import br.com.treinaweb.twclientes.controller.response.dto.ClienteDto;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,5 +31,24 @@ public class ClienteController {
 
         return modelAndView;
     }
+
+    @GetMapping("/{id}")
+    public ModelAndView detalhar(@PathVariable Long id) throws Throwable {
+        ModelAndView modelAndView = new ModelAndView("cliente/detalhar.html");
+
+        modelAndView.addObject("cliente", clienteService.detalhar(id));
+
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}/excluir")
+    public ModelAndView excluir(@PathVariable Long id) throws Exception {
+        ModelAndView modelAndView = new ModelAndView("redirect:/cliente");
+
+        clienteService.excluir(id);
+
+        return modelAndView;
+    }
+
     
 }
